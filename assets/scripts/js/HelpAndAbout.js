e50aeb3e034c2ad1360ea4371ea279d576e77c3b
CallumMartinA <111565498+CallumMartinA@users.noreply.github.com>
2022-08-26 11:20:46 +0100

    Add files via upload
    Added missing punctuation to content.

--- assets/scripts/js/HelpAndAbout.js
+++ assets/scripts/js/HelpAndAbout.js
@@ -7,10 +7,10 @@ function GetHelpAndAboutContent() {
 
     let Content = `
         <div class="HelpAndAboutContainer">
-            <p><span>Project:</span> Community helper</p>
-            <p><span>Version:</span> ${GetVersion()}</p>
-            <p><span>Author:</span> Callum Martin</p>
-            <p><span>Admins:</span> Callum Martin</p>
+            <p><span>Project:</span> Community helper.</p>
+            <p><span>Version:</span> ${GetVersion()}.</p>
+            <p><span>Author:</span> Callum Martin.</p>
+            <p><span>Admins:</span> Callum Martin.</p>
             <p><span>Description:</span> Aligns the input and output of MDS.</p>
             <p><span>Report an issue:</span> Contact an admin via email.</p>
         </div>
